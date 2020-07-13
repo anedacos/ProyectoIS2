00a2f53e264284356c9c8ad5b02f07facf65da36
llimap <lejalima@espol.edu.ec>
2020-07-13 15:14:10 -0500

    avance proyecto

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -11,10 +11,10 @@ import { IconsComponent } from "../../icons/icons.component";
 import { MapsComponent } from "../../maps/maps.component";
 import { NotificationsComponent } from "../../notifications/notifications.component";
 import { UpgradeComponent } from "../../upgrade/upgrade.component";
-
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
 import { MainComponent } from "../../viaticos/main/main.component";
 import { CreateOrEditComponent } from "../../viaticos/create-or-edit/create-or-edit.component";
-
+// import {MatStartDate } from "@angular/material/Start";
 import { MatButtonModule } from "@angular/material/button";
 import { MatInputModule } from "@angular/material/input";
 import { MatRippleModule } from "@angular/material/core";
@@ -22,7 +22,13 @@ import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatCheckboxModule } from "@angular/material/checkbox";
 import { MatTooltipModule } from "@angular/material/tooltip";
 import { MatSelectModule } from "@angular/material/select";
-
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+// import { MatDaterangepickerModule } from "@angular/material/datepicker";
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatSortModule } from "@angular/material/sort";
 @NgModule({
   imports: [
     CommonModule,
@@ -36,6 +42,13 @@ import { MatSelectModule } from "@angular/material/select";
     MatSelectModule,
     MatTooltipModule,
     MatCheckboxModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatAutocompleteModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatDialogModule,
+    MatSortModule,
   ],
   declarations: [
     DashboardComponent,
